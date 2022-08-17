b9b62c01b1adf264572b22c75e21511a12aa3701
zqhong <zqh0513@gmail.com>
2022-08-17 11:17:26 +0800

    improve：改用 Vanara.PInvoke.PowrProf 设置电源计划选项

---
+++ PowerConfig.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.PowrProf;
+
+namespace RyzenTuner
+{
+    public class PowerConfig
+    {
+        private const uint Disabled = 0;
+        private const uint Enabled = 1;
+
+        /**
+         * 获取当前激活的电源方案的 GUID
+         */
+        private Guid GetActiveScheme()
+        {
+            HKEY powerKey = default;
+            var result = PowerGetActiveScheme(powerKey, out var activeGuidHandle);
+            if (result != Win32Error.NO_ERROR)
+            {
+                throw new Exception(result.ToString());
+            }
+
+            return activeGuidHandle.ToStructure<Guid>();
+        }
+
+        /**
+         * 检查是否开启了 Cpu Boost
+         */
+        private bool IsEnableCpuBoost()
+        {
+            HKEY powerKey = default;
+            var activeGuid = GetActiveScheme();
+
+            PowerReadACValueIndex(
+                powerKey,
+                activeGuid,
+                GUID_PROCESSOR_SETTINGS_SUBGROUP,
+                GUID_PROCESSOR_PERF_BOOST_MODE,
+                out var acValue
+            );
+
+            PowerReadDCValueIndex(
+                powerKey,
+                activeGuid,
+                GUID_PROCESSOR_SETTINGS_SUBGROUP,
+                GUID_PROCESSOR_PERF_BOOST_MODE,
+                out var dcValue
+            );
+
+            return acValue == Enabled && dcValue == Enabled;
+        }
+
+        /**
+         * 启用 Cpu Boost
+         */
+        public bool EnableCpuBoost()
+        {
+            if (IsEnableCpuBoost())
+            {
+                return true;
+            }
+
+            HKEY powerKey = default;
+            var activeGuid = GetActiveScheme();
+
+            var r1 = PowerWriteACValueIndex(
+                powerKey,
+                activeGuid,
+                GUID_PROCESSOR_SETTINGS_SUBGROUP,
+                GUID_PROCESSOR_PERF_BOOST_MODE,
+                Enabled
+            );
+
+            var r2 = PowerWriteDCValueIndex(
+                powerKey,
+                activeGuid,
+                GUID_PROCESSOR_SETTINGS_SUBGROUP,
+                GUID_PROCESSOR_PERF_BOOST_MODE,
+                Enabled
+            );
+
+            var r3 = PowerSetActiveScheme(powerKey, activeGuid);
+
+            return r1 == Win32Error.NO_ERROR && r2 == Win32Error.NO_ERROR && r3 == Win32Error.NO_ERROR;
+        }
+
+        /**
+         * 关闭 Cpu Boost
+         */
+        public bool DisableCpuBoost()
+        {
+            if (!IsEnableCpuBoost())
+            {
+                return true;
+            }
+
+            HKEY powerKey = default;
+            var activeGuid = GetActiveScheme();
+
+            var r1 = PowerWriteACValueIndex(
+                powerKey,
+                activeGuid,
+                GUID_PROCESSOR_SETTINGS_SUBGROUP,
+                GUID_PROCESSOR_PERF_BOOST_MODE,
+                Disabled
+            );
+
+            var r2 = PowerWriteDCValueIndex(
+                powerKey,
+                activeGuid,
+                GUID_PROCESSOR_SETTINGS_SUBGROUP,
+                GUID_PROCESSOR_PERF_BOOST_MODE,
+                Disabled
+            );
+
+            var r3 = PowerSetActiveScheme(powerKey, activeGuid);
+            
+            return r1 == Win32Error.NO_ERROR && r2 == Win32Error.NO_ERROR && r3 == Win32Error.NO_ERROR;
+        }
+    }
+}
