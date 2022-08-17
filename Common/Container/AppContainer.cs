a881944e60878ead539a3ac14829aa168381f25c
zqhong <zqh0513@gmail.com>
2022-08-17 21:45:15 +0800

    refactor：新增 container，重新组织代码

---
+++ Common/Container/AppContainer.cs
@@ -0,0 +1,41 @@
+﻿using RyzenTuner.Common.Processor;
+
+namespace RyzenTuner.Common.Container
+{
+    /**
+     * Refer: https://csharpindepth.com/articles/singleton#cctor
+     */
+    public static class AppContainer
+    {
+        private static readonly Container Container;
+
+        static AppContainer()
+        {
+            Container = new Container();
+
+            Container.Register(() => new HardwareMonitor())
+                .AsSingleton();
+
+            Container.Register(() => new PowerConfig())
+                .AsSingleton();
+
+            Container.Register(() => new AmdProcessor())
+                .AsSingleton();
+        }
+
+        public static HardwareMonitor HardwareMonitor()
+        {
+            return Container.Resolve<HardwareMonitor>();
+        }
+
+        public static PowerConfig PowerConfig()
+        {
+            return Container.Resolve<PowerConfig>();
+        }
+
+        public static AmdProcessor AmdProcessor()
+        {
+            return Container.Resolve<AmdProcessor>();
+        }
+    }
+}
