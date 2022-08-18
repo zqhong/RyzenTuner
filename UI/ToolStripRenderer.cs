15e0b643e42d57aca6e5b7f13b0c818956e63ddb
zqhong <zqh0513@gmail.com>
2022-08-18 16:53:39 +0800

    improve: 改善托盘菜单项选中效果

---
+++ UI/ToolStripRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace RyzenTuner.UI
+{
+    public class ToolStripRenderer : ToolStripProfessionalRenderer
+    {
+        protected override void OnRenderItemText(ToolStripItemTextRenderEventArgs e)
+        {
+            if (e.Item is ToolStripMenuItem { Checked: true } item)
+            {
+                e.TextFont = new Font(item.Font, FontStyle.Bold);
+            }
+
+            base.OnRenderItemText(e);
+        }
+    }
+}
