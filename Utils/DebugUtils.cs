7519c418523470534fbb7b62e59daba8635e562c
zqhong <zqh0513@gmail.com>
2022-08-18 20:44:22 +0800

    added: 新增 DebugUtils.LogCpuInfo()，用于测试

--- Utils/DebugUtils.cs
+++ Utils/DebugUtils.cs
@@ -2,15 +2,40 @@
 
 namespace RyzenTuner.Utils
 {
-    public class DebugUtils
+    public static class DebugUtils
     {
-        public void Test()
+        /// <summary>
+        /// 记录 Cpu 在不同限制功耗下的数据
+        ///
+        /// 备注：仅在开发使用
+        /// </summary>
+        public static void LogCpuInfo()
         {
             AppContainer.HardwareMonitor().Monitor();
-            
-            for (var i = 1; i <= 30; i++)
+
+            AppContainer.PowerConfig().DisableCpuBoost();
+            AppContainer.Logger().Debug("关闭睿频");
+            _logCpuInfo();
+
+            AppContainer.PowerConfig().EnableCpuBoost();
+            AppContainer.Logger().Debug("开启睿频");
+            _logCpuInfo();
+        }
+
+        private static void _logCpuInfo()
+        {
+            const int maxPowerLimit = 30;
+            var hardware = AppContainer.HardwareMonitor();
+
+            for (var i = 1; i <= maxPowerLimit; i++)
             {
-                
+                AppContainer.AmdProcessor().SetAllTdpLimit(i);
+                System.Threading.Thread.Sleep(1000);
+                AppContainer.HardwareMonitor().Monitor();
+
+                AppContainer.Logger()
+                    .Debug(
+                        $"限制{i}瓦，CPU：{hardware.CpuUsage}%、{hardware.CpuTemperature}℃、{hardware.CpuFreq}MHz，GPU：{hardware.VideoCard3DUsage}");
             }
         }
     }
