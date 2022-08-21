1615f94a9ee05a865ea7ca809f88997890f92ab1
zqhong <zqh0513@gmail.com>
2022-08-21 21:15:22 +0800

    added: 内置 EnergyStar

---
+++ Common/EnergyStar/EnergyManager.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using RyzenTuner.Common.EnergyStar.Interop;
+
+namespace RyzenTuner.Common.EnergyStar
+{
+    public class EnergyManager
+    {
+        private static readonly HashSet<string> BypassProcessList = new HashSet<string>
+        {
+            // Visual Studio
+            "devenv.exe",
+        };
+
+        // Special handling needs for UWP to get the child window process
+        private const string UwpFrameHostApp = "ApplicationFrameHost.exe";
+
+        private static uint _pendingProcPid = 0;
+        private static string _pendingProcName = "";
+
+        private static readonly IntPtr PThrottleOn;
+        private static readonly IntPtr PThrottleOff;
+        private static readonly int SzControlBlock = 0;
+
+        static EnergyManager()
+        {
+            // TODO：Setting 类没有实现，暂时屏蔽
+            // var settings = Settings.Load();
+            // BypassProcessList.UnionWith(settings.Exemptions.Select(x => x.ToLowerInvariant()));
+
+            SzControlBlock = Marshal.SizeOf<Win32Api.ProcessPowerThrottlingState>();
+            PThrottleOn = Marshal.AllocHGlobal(SzControlBlock);
+            PThrottleOff = Marshal.AllocHGlobal(SzControlBlock);
+
+            var throttleState = new Win32Api.ProcessPowerThrottlingState
+            {
+                Version = Win32Api.ProcessPowerThrottlingState.ProcessPowerThrottlingCurrentVersion,
+                ControlMask = Win32Api.ProcessorPowerThrottlingFlags.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+                StateMask = Win32Api.ProcessorPowerThrottlingFlags.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+            };
+
+            var unthrottleState = new Win32Api.ProcessPowerThrottlingState
+            {
+                Version = Win32Api.ProcessPowerThrottlingState.ProcessPowerThrottlingCurrentVersion,
+                ControlMask = Win32Api.ProcessorPowerThrottlingFlags.PROCESS_POWER_THROTTLING_EXECUTION_SPEED,
+                StateMask = Win32Api.ProcessorPowerThrottlingFlags.None,
+            };
+
+            Marshal.StructureToPtr(throttleState, PThrottleOn, false);
+            Marshal.StructureToPtr(unthrottleState, PThrottleOff, false);
+        }
+
+        private static void ToggleEfficiencyMode(IntPtr hProcess, bool enable)
+        {
+            Win32Api.SetProcessInformation(hProcess, Win32Api.PROCESS_INFORMATION_CLASS.ProcessPowerThrottling,
+                enable ? PThrottleOn : PThrottleOff, (uint)SzControlBlock);
+            Win32Api.SetPriorityClass(hProcess,
+                enable ? Win32Api.PriorityClass.IDLE_PRIORITY_CLASS : Win32Api.PriorityClass.NORMAL_PRIORITY_CLASS);
+        }
+
+        private static string GetProcessNameFromHandle(IntPtr hProcess)
+        {
+            var capacity = 1024;
+            var sb = new StringBuilder(capacity);
+
+            if (Win32Api.QueryFullProcessImageName(hProcess, 0, sb, ref capacity))
+            {
+                return Path.GetFileName(sb.ToString());
+            }
+
+            return "";
+        }
+
+        public static void HandleForegroundEvent(IntPtr hwnd)
+        {
+            var windowThreadId = Win32Api.GetWindowThreadProcessId(hwnd, out var procId);
+            
+            // This is invalid, likely a process is dead, or idk
+            if (windowThreadId == 0 || procId == 0) return;
+
+            var procHandle = Win32Api.OpenProcess(
+                (uint)(Win32Api.ProcessAccessFlags.QueryLimitedInformation |
+                       Win32Api.ProcessAccessFlags.SetInformation), false, procId);
+            if (procHandle == IntPtr.Zero) return;
+
+            // Get the process
+            var appName = GetProcessNameFromHandle(procHandle);
+
+            // UWP needs to be handled in a special case
+            if (appName == UwpFrameHostApp)
+            {
+                var found = false;
+                Win32Api.EnumChildWindows(hwnd, (innerHwnd, lparam) =>
+                {
+                    if (found) return true;
+                    if (Win32Api.GetWindowThreadProcessId(innerHwnd, out uint innerProcId) > 0)
+                    {
+                        if (procId == innerProcId) return true;
+
+                        var innerProcHandle = Win32Api.OpenProcess(
+                            (uint)(Win32Api.ProcessAccessFlags.QueryLimitedInformation |
+                                   Win32Api.ProcessAccessFlags.SetInformation), false, innerProcId);
+                        if (innerProcHandle == IntPtr.Zero) return true;
+
+                        // Found. Set flag, reinitialize handles and call it a day
+                        found = true;
+                        Win32Api.CloseHandle(procHandle);
+                        procHandle = innerProcHandle;
+                        procId = innerProcId;
+                        appName = GetProcessNameFromHandle(procHandle);
+                    }
+
+                    return true;
+                }, IntPtr.Zero);
+            }
+
+            // Boost the current foreground app, and then impose EcoQoS for previous foreground app
+            var bypass = BypassProcessList.Contains(appName.ToLowerInvariant());
+            if (!bypass)
+            {
+                Console.WriteLine($"Boost {appName}");
+                ToggleEfficiencyMode(procHandle, false);
+            }
+
+            if (_pendingProcPid != 0)
+            {
+                Console.WriteLine($"Throttle {_pendingProcName}");
+
+                var prevProcHandle = Win32Api.OpenProcess((uint)Win32Api.ProcessAccessFlags.SetInformation, false,
+                    _pendingProcPid);
+                if (prevProcHandle != IntPtr.Zero)
+                {
+                    ToggleEfficiencyMode(prevProcHandle, true);
+                    Win32Api.CloseHandle(prevProcHandle);
+                    _pendingProcPid = 0;
+                    _pendingProcName = "";
+                }
+            }
+
+            if (!bypass)
+            {
+                _pendingProcPid = procId;
+                _pendingProcName = appName;
+            }
+
+            Win32Api.CloseHandle(procHandle);
+        }
+
+        public static void ThrottleAllUserBackgroundProcesses()
+        {
+            var runningProcesses = Process.GetProcesses();
+            var currentSessionId = Process.GetCurrentProcess().SessionId;
+
+            var sameAsThisSession = runningProcesses.Where(p => p.SessionId == currentSessionId);
+            foreach (var proc in sameAsThisSession)
+            {
+                if (proc.Id == _pendingProcPid)
+                {
+                    continue;
+                }
+
+                if (BypassProcessList.Contains($"{proc.ProcessName}.exe".ToLowerInvariant())) continue;
+                var hProcess = Win32Api.OpenProcess((uint)Win32Api.ProcessAccessFlags.SetInformation, false,
+                    (uint)proc.Id);
+                ToggleEfficiencyMode(hProcess, true);
+                Win32Api.CloseHandle(hProcess);
+            }
+        }
+    }
+}
---
+++ Common/EnergyStar/Interop/Win32Api.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace RyzenTuner.Common.EnergyStar.Interop
+{
+    internal class Win32Api
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, uint processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool QueryFullProcessImageName([In] IntPtr hProcess, [In] int dwFlags, [Out] StringBuilder lpExeName, ref int lpdwSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetProcessInformation([In] IntPtr hProcess,
+            [In] PROCESS_INFORMATION_CLASS ProcessInformationClass, IntPtr ProcessInformation, uint ProcessInformationSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetPriorityClass(IntPtr handle, PriorityClass priorityClass);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        public delegate bool WindowEnumProc(IntPtr hwnd, IntPtr lparam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool EnumChildWindows(IntPtr hwnd, WindowEnumProc callback, IntPtr lParam);
+
+        // We don't need to bloat this app with WinForm/WPF to show a simple message box
+        [DllImport("user32.dll")]
+        public static extern int MessageBox(IntPtr hInstance, string lpText, string lpCaption, uint type);
+
+        // two message box releated constants
+        public const int MB_OK = 0x00000000;
+        public const int MB_ICONERROR = 0x00000010;
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        public enum PROCESS_INFORMATION_CLASS
+        {
+            ProcessMemoryPriority,
+            ProcessMemoryExhaustionInfo,
+            ProcessAppMemoryInfo,
+            ProcessInPrivateInfo,
+            ProcessPowerThrottling,
+            ProcessReservedValue1,
+            ProcessTelemetryCoverageInfo,
+            ProcessProtectionLevelInfo,
+            ProcessLeapSecondInfo,
+            ProcessInformationClassMax,
+        }
+
+        [Flags]
+        public enum ProcessorPowerThrottlingFlags : uint
+        {
+            None = 0x0,
+            PROCESS_POWER_THROTTLING_EXECUTION_SPEED = 0x1,
+        }
+
+        public enum PriorityClass : uint
+        {
+            ABOVE_NORMAL_PRIORITY_CLASS = 0x8000,
+            BELOW_NORMAL_PRIORITY_CLASS = 0x4000,
+            HIGH_PRIORITY_CLASS = 0x80,
+            IDLE_PRIORITY_CLASS = 0x40,
+            NORMAL_PRIORITY_CLASS = 0x20,
+            PROCESS_MODE_BACKGROUND_BEGIN = 0x100000,// 'Windows Vista/2008 and higher
+            PROCESS_MODE_BACKGROUND_END = 0x200000,//   'Windows Vista/2008 and higher
+            REALTIME_PRIORITY_CLASS = 0x100
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ProcessPowerThrottlingState
+        {
+            public const uint ProcessPowerThrottlingCurrentVersion = 1;
+
+            public uint Version;
+            public ProcessorPowerThrottlingFlags ControlMask;
+            public ProcessorPowerThrottlingFlags StateMask;
+        }
+    }
+}
