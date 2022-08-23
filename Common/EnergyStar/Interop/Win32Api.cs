971e81c1376c2d915bef49e47c99d4085948f5f4
zqhong <zqh0513@gmail.com>
2022-08-23 23:29:03 +0800

    docs: 添加 EnergyStar 的代码备注

--- Common/EnergyStar/Interop/Win32Api.cs
+++ Common/EnergyStar/Interop/Win32Api.cs
@@ -14,11 +14,13 @@ namespace RyzenTuner.Common.EnergyStar.Interop
         public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, uint processId);
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool QueryFullProcessImageName([In] IntPtr hProcess, [In] int dwFlags, [Out] StringBuilder lpExeName, ref int lpdwSize);
+        public static extern bool QueryFullProcessImageName([In] IntPtr hProcess, [In] int dwFlags,
+            [Out] StringBuilder lpExeName, ref int lpdwSize);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool SetProcessInformation([In] IntPtr hProcess,
-            [In] PROCESS_INFORMATION_CLASS ProcessInformationClass, IntPtr ProcessInformation, uint ProcessInformationSize);
+            [In] PROCESS_INFORMATION_CLASS ProcessInformationClass, IntPtr ProcessInformation,
+            uint ProcessInformationSize);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool SetPriorityClass(IntPtr handle, PriorityClass priorityClass);
@@ -60,12 +62,18 @@ namespace RyzenTuner.Common.EnergyStar.Interop
             Synchronize = 0x00100000
         }
 
+        /// <summary>
+        /// 参考：https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ne-processthreadsapi-process_information_class
+        /// </summary>
         public enum PROCESS_INFORMATION_CLASS
         {
             ProcessMemoryPriority,
             ProcessMemoryExhaustionInfo,
             ProcessAppMemoryInfo,
             ProcessInPrivateInfo,
+
+            // The process information is represented by a PROCESS_POWER_THROTTLING_STATE structure.
+            // Allows applications to configure how the system should throttle the target process’s activity when managing power.
             ProcessPowerThrottling,
             ProcessReservedValue1,
             ProcessTelemetryCoverageInfo,
@@ -81,15 +89,37 @@ namespace RyzenTuner.Common.EnergyStar.Interop
             PROCESS_POWER_THROTTLING_EXECUTION_SPEED = 0x1,
         }
 
+        /// <summary>
+        /// 参考：https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-setpriorityclass
+        /// </summary>
         public enum PriorityClass : uint
         {
+            // 优先级高于普通优先级但低于高优先级的进程
             ABOVE_NORMAL_PRIORITY_CLASS = 0x8000,
+
+            // 优先级高于空闲优先级但低于正常优先级的进程
             BELOW_NORMAL_PRIORITY_CLASS = 0x4000,
+
+            // 执行必须立即执行的时间关键任务的流程
+            // 在使用高优先级类时要格外小心，因为高优先级类应用程序几乎可以使用所有可用的CPU时间。
             HIGH_PRIORITY_CLASS = 0x80,
+
+            // 仅在系统空闲时才运行线程的进程。进程的线程会被运行在更高优先级类中的任何进程的线程抢占。
+            // 屏幕保护程序就是一个例子。空闲优先级类由子进程继承
             IDLE_PRIORITY_CLASS = 0x40,
+
+            // Process with no special scheduling needs
             NORMAL_PRIORITY_CLASS = 0x20,
-            PROCESS_MODE_BACKGROUND_BEGIN = 0x100000,// 'Windows Vista/2008 and higher
-            PROCESS_MODE_BACKGROUND_END = 0x200000,//   'Windows Vista/2008 and higher
+            
+            // 开始后台处理模式。系统降低了进程(及其线程)的资源调度优先级，以便它可以在不显著影响前台活动的情况下执行后台工作
+            // PROCESS_MODE_BACKGROUND_BEGIN 不适合用于低优先级的后台工作
+            // 参考：https://stackoverflow.com/questions/13631644/setthreadpriority-and-setpriorityclass
+            PROCESS_MODE_BACKGROUND_BEGIN = 0x100000, // Windows Vista/2008 and higher
+            
+            // 结束后台处理模式。系统恢复进程(及其线程)进入后台处理模式前的资源调度优先级
+            PROCESS_MODE_BACKGROUND_END = 0x200000, //   Windows Vista/2008 and higher
+            
+            // 具有最高优先级的进程。该进程的线程抢占所有其他进程的线程，包括执行重要任务的操作系统进程
             REALTIME_PRIORITY_CLASS = 0x100
         }
 
