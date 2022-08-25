cb9e8262e37b08f6a957f29f0eef6a81dd0c8ce3
zqhong <zqh0513@gmail.com>
2022-08-25 19:06:46 +0800

    updated: release 版本 log level 设置为 warning

--- Common/EnergyStar/Interop/Win32Api.cs
+++ Common/EnergyStar/Interop/Win32Api.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security;
-using System.Text;
 
 namespace RyzenTuner.Common.EnergyStar.Interop
 {
@@ -17,9 +15,6 @@ namespace RyzenTuner.Common.EnergyStar.Interop
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, uint processId);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool QueryFullProcessImageName([In] IntPtr hProcess, [In] int dwFlags,
-            [Out] StringBuilder lpExeName, ref int lpdwSize);
 
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern bool SetProcessInformation([In] IntPtr hProcess,
@@ -107,22 +102,25 @@ namespace RyzenTuner.Common.EnergyStar.Interop
         /// </summary>
         public enum PriorityClass : uint
         {
-            // 优先级高于普通优先级但低于高优先级的进程
-            ABOVE_NORMAL_PRIORITY_CLASS = 0x8000,
+            // 【实时】具有最高优先级的进程。该进程的线程抢占所有其他进程的线程，包括执行重要任务的操作系统进程
+            REALTIME_PRIORITY_CLASS = 0x100,
 
-            // 优先级高于空闲优先级但低于正常优先级的进程
-            BELOW_NORMAL_PRIORITY_CLASS = 0x4000,
-
-            // 执行必须立即执行的时间关键任务的流程
+            // 【高】执行必须立即执行的时间关键任务的流程
             // 在使用高优先级类时要格外小心，因为高优先级类应用程序几乎可以使用所有可用的CPU时间。
             HIGH_PRIORITY_CLASS = 0x80,
 
-            // 仅在系统空闲时才运行线程的进程。进程的线程会被运行在更高优先级类中的任何进程的线程抢占。
-            // 屏幕保护程序就是一个例子。空闲优先级类由子进程继承
-            IDLE_PRIORITY_CLASS = 0x40,
+            // 【高于正常】优先级高于普通优先级但低于高优先级的进程
+            ABOVE_NORMAL_PRIORITY_CLASS = 0x8000,
 
-            // Process with no special scheduling needs
-            NORMAL_PRIORITY_CLASS = 0x20,
+            // 【正常】Process with no special scheduling needs
+            NORMAL_PRIORITY_CLASS = 0x00000020,
+
+            // 【低于正常】优先级高于空闲优先级但低于正常优先级的进程
+            BELOW_NORMAL_PRIORITY_CLASS = 0x4000,
+
+            // 【低】仅在系统空闲时才运行线程的进程。进程的线程会被运行在更高优先级类中的任何进程的线程抢占。
+            // 屏幕保护程序就是一个例子。空闲优先级类由子进程继承
+            IDLE_PRIORITY_CLASS = 0x00000040,
 
             // 开始后台处理模式。系统降低了进程(及其线程)的资源调度优先级，以便它可以在不显著影响前台活动的情况下执行后台工作
             // PROCESS_MODE_BACKGROUND_BEGIN 不适合用于低优先级的后台工作
@@ -131,9 +129,6 @@ namespace RyzenTuner.Common.EnergyStar.Interop
 
             // 结束后台处理模式。系统恢复进程(及其线程)进入后台处理模式前的资源调度优先级
             PROCESS_MODE_BACKGROUND_END = 0x200000, //   Windows Vista/2008 and higher
-
-            // 具有最高优先级的进程。该进程的线程抢占所有其他进程的线程，包括执行重要任务的操作系统进程
-            REALTIME_PRIORITY_CLASS = 0x100
         }
 
         [StructLayout(LayoutKind.Sequential)]
