3aed4c07835adfd9488cd758bd71a34bc447aa9c
zqhong <zqh0513@gmail.com>
2022-08-25 15:56:10 +0800

    Bump version to 1.2.0.302

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      次版本
 //      生成号
 //      修订号
-[assembly: AssemblyVersion("1.2.0.301")]
-[assembly: AssemblyFileVersion("1.2.0.301")]
+[assembly: AssemblyVersion("1.2.0.302")]
+[assembly: AssemblyFileVersion("1.2.0.302")]
