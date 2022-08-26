b7a2c92779f5b6fce99c1b3beb38549e0cf8654c
zqhong <zqh0513@gmail.com>
2022-08-26 15:07:00 +0800

    improve: 添加对未捕获异常的处理

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
+using RyzenTuner.Common.Container;
 using RyzenTuner.UI;
 
 namespace RyzenTuner
@@ -9,6 +11,9 @@ namespace RyzenTuner
         [STAThread]
         private static void Main()
         {
+            Application.ThreadException += Application_ThreadException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             if (Environment.OSVersion.Version.Major >= 6)
             {
                 SetProcessDPIAware();
@@ -17,6 +22,27 @@ namespace RyzenTuner
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            _handleUnhandledException(e.Exception);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs arg)
+        {
+            var e = (Exception)arg.ExceptionObject;
+            _handleUnhandledException(e);
+        }
+
+        private static void _handleUnhandledException(Exception ex)
+        {
+            MessageBox.Show(ex.Message, "RyzenTuner 出现错误",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            AppContainer.Logger().LogException(ex);
+
+            Application.Exit();
         }
 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
